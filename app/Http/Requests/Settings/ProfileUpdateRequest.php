f1be4397a60a6152b500d714773f38f41a8b341e
Nicolas BACON <nicolasbacon.nb@gmail.com>
2026-02-05 18:56:52 +0100

    Develop to main (#4)
    * feat: adding roles for users
    
    * fix: add default role when register
    
    * feat: admin can edit his role
    
    * feat: missing Policy
    
    * feat: trad for role

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -3,12 +3,19 @@
 namespace App\Http\Requests\Settings;
 
 use App\Concerns\ProfileValidationRules;
+use App\Enums\Roles;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
     use ProfileValidationRules;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
 
     /**
      * Get the validation rules that apply to the request.
@@ -17,6 +24,18 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        return $this->profileRules($this->user()->id);
+        $canUpdateRole = $this->user()->can('updateRole', $this->user());
+
+        return [
+            ...$this->profileRules($this->user()->id),
+            'role' => [
+                Rule::when($canUpdateRole, [
+                    'required',
+                    Rule::enum(Roles::class),
+                ], [
+                    'prohibited',
+                ]),
+            ],
+        ];
     }
 }
